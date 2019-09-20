103cd23e1f653a3d3edfaaf64c7c5f95091ef777
mrlhz <mr__lhz@163.com>
2019-09-20 15:45:00 +0800

    save the webpage content as an html file

--- tutorial/savehtml.js
+++ tutorial/savehtml.js
@@ -7,9 +7,11 @@ const axios = require('axios')
 require('module-alias/register')
 
 const { sleep, mkdirSync, writeFile } = require('../src/helper/tools')
+const { Browser } = require('../src/helper/browser')
 const { executablePath } = require('@config/index')
 
-async function saveHtml(urls, selector) {
+async function saveHtml(urls, handleHtmlFunc, selector, output, titles = []) {
+  console.time('time')
   let len = urls.length
   // for (let i = 0; i < len; i++) {
   //   let html = await axios.get(urls[i])
@@ -41,53 +43,122 @@ async function saveHtml(urls, selector) {
 
       await sleep(3000)
       // await page.emulateMedia('screen')
-      let title = await page.title()
-      title = (1+i) + '-' + title.replace(/[\\\/\:\*\?\"\<\>\|]/g, '-')
-      let result = await page.evaluate((selector) => {
-        document.querySelector('.page-nav').innerHTML = '';
-        return document.querySelector(selector).innerHTML
-      }, selector)
+      let title = titles[i]
+      if(!title) {
+        title = await page.title()
+        title = (1 + i) + '-' + title.replace(/[\\\/\:\*\?\"\<\>\|]/g, '-')
+      }
 
-      result = result.replace(/<span class="line-number">\d*<\/span>/g, '')
-                     .replace(/<br>/g, '')
-                     .replace('<p><img src="https:\/\/www\.fullstackjavascript\.cn\/wx\.png" alt=""><\/p>', '')
-      writeFile(title + '.html', result,{
-        output: 'D:/books/mdn/html'
+      const result = await handleHtmlFunc(page, selector)
+      writeFile(title + '.html', result, {
+        output
       })
 
       console.log(`${c.bgGreen('done')} ${(i + 1)}/${len}`)
 
-      await sleep(3000)
+      await sleep(2000)
 
       await page.close()
     } catch (e) {
       console.log(e)
     }
   }
-
   await browser.close()
+  console.timeEnd('time')
 }
 
+async function getTrainVueInfoUrls(page) {
+  return await page.evaluate(() => {
+    let urls = []
+    Array.from(document.querySelectorAll('.sidebar-links li > a.sidebar-link')).forEach((item) => {
+      let url = item.getAttribute('href')
+      if (url.indexOf('#') === -1) {
+        urls.push(location.origin + url)
+      }
+    })
+    return urls
+  })
+}
+
+async function getTrainVueInfoPage(page, selector) {
+  let result = await page.evaluate((selector) => {
+    document.querySelector('.page-nav').innerHTML = '';
+    return document.querySelector(selector).innerHTML
+  }, selector)
+
+  result = result.replace(/<span class="line-number">\d*<\/span>/g, '')
+    .replace(/<br>/g, '')
+    .replace('<p><img src="https:\/\/www\.fullstackjavascript\.cn\/wx\.png" alt=""><\/p>', '');
+  return result;
+}
+
+async function getAheadUrls(page) {
+  const urls = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll('.nav ul li > a')).map((item) => {
+      const href = item.getAttribute('href')
+      return {
+        title: item.innerText,
+        url: 'http://www.zhufengpeixun.cn/ahead/' + href
+      }
+    })
+  })
+  return urls
+}
+
+async function getSelectorPage(page, selector) {
+  return await page.evaluate((selector) => {
+    const content = document.querySelector(selector)
+    return content ? content.innerHTML : ''
+  }, selector)
+}
+
+async function getSomeFiles(urls, handleHtmlFunc, file) {
+  let items = []
+  const instance = new Browser({
+    headless: false
+  })
+  for (let i = 0; i < urls.length; i++) {
+    const page = await instance.goto(urls[i])
+    try {
+      const item = await handleHtmlFunc(page)
+      if (item.length === 0) {
+        break
+      }
+      items.push(...item)
+      await page.close()
+    } catch (e) {
+      console.log(e)
+    }
+  }
+  writeFile(file, items, {
+    output: path.resolve(__dirname)
+  })
+
+  await instance.close()
+}
 
 // mkdirSync('D:/books/mdn/html')
 
 /**
  * `bug css样式丢失`
  */
-// saveHtml(['https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array'])
+const urls = ['https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array']
+saveHtml(urls, getSelectorPage, '#wikiArticle', 'D:/books/mdn/html')
 
+/**
+ * 1. run getTrainVueInfoUrls
+ * 2. `http://www.zhufengpeixun.cn/train/vue-info/component.html`
+ */
+// getSomeFiles(['http://www.zhufengpeixun.cn/train/vue-info/component.html'], getTrainVueInfoUrls, 'train-vue-info-urls.json')
+// const urls = require('./train-vue-info-urls.json')
+// saveHtml(urls.slice(0, 1), getTrainVueInfoPage, 'main.page', 'D:/books/mdn/html/train-vue-info')
 
-let urls = ["http://www.zhufengpeixun.cn/train/vue-info/component.html", "http://www.zhufengpeixun.cn/train/vue-info/jsx.html", "http://www.zhufengpeixun.cn/train/vue-info/jwt.html", "http://www.zhufengpeixun.cn/train/vue-info/cascader.html", "http://www.zhufengpeixun.cn/train/vue-info/unit.html", "http://www.zhufengpeixun.cn/train/vue-info/auth.html", "http://www.zhufengpeixun.cn/train/vue-info/vue-router.html", "http://www.zhufengpeixun.cn/train/vue-info/source.html", "http://www.zhufengpeixun.cn/train/vue-info/ssr.html", "http://www.zhufengpeixun.cn/train/vue-info/vue+ts.html"]
-function getUrls() {
-  let urls = []
-  Array.from(document.querySelectorAll('.sidebar-links li > a.sidebar-link')).forEach((item) => {
-    let url = item.getAttribute('href')
-    if (url.indexOf('#') === -1) {
-      urls.push(location.origin + url)
-    }
-  })
-  console.log(urls)
-  return urls
-}
+/**
+ * `http://www.zhufengpeixun.cn/ahead/index.html`
+ */
+// getSomeFiles(['http://www.zhufengpeixun.cn/ahead/index.html'], getAheadUrls, 'ahead-urls.json')
 
-saveHtml(urls, 'main.page')
+// let urls = require('./ahead-urls.json')
+// const titles = urls.map((item) => item.title)
+// urls = urls.map((item) => item.url)
+// saveHtml(urls, getSelectorPage, '.content.markdown-body', 'D:/books/mdn/html/ahead', titles)
