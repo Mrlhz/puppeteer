0f9658ecdd28a3d796f197f33a1265e908284409
mrlhz <mr__lhz@163.com>
2019-12-05 23:43:24 +0800

    refactor: get picture

--- src/helper/getJSONFile.js
+++ src/helper/getJSONFile.js
@@ -1,3 +1,4 @@
+const util = require('util')
 const c = require('ansi-colors')
 
 const { Browser } = require('./browser')
@@ -42,17 +43,24 @@ async function index(urls = [], name = Date.now(), dir = __dirname) {
 async function getHtml(page) {
   try {
     const item = await page.evaluate(() => {
-      return Array.from(document.querySelectorAll('.boxs .img li')).map((item) => {
-        let p = item.querySelectorAll('p')
-        let a = item.querySelector('a')
-
-        return {
-          p: Number.parseInt(p[0].innerText.match(/\d+/)[0]),
-          dir: p[4].innerText.replace('[@crepe]', ''), // remove [@crepe]
-          url: a ? a.getAttribute('href').match(/\d{4}/)[0] : '',
-          tags: Array.from(p[3].querySelectorAll('a')).map((tag) => tag.innerText)
-        }
-      })
+      function html() {
+        return Array.from(document.querySelectorAll('.boxs .img li')).map((item) => {
+          let p = item.querySelectorAll('p')
+          let a = item.querySelector('a')
+  
+          return {
+            p: Number.parseInt(p[0].innerText.match(/\d+/)[0]),
+            dir: p[4].innerText, // remove [@crepe]
+            url: a ? a.getAttribute('href').match(/(\d+)/)[1] : '',
+            tags: Array.from(p[3].querySelectorAll('a')).map((tag) => tag.innerText)
+          }
+        })
+      }
+      const total = html().reduce((acc, cur) => {
+        acc += cur.p
+        return acc
+      }, 0)
+      return html()
     })
     return item
   } catch (e) {
@@ -73,4 +81,23 @@ async function _html(page) {
 }
 
 // test
-index(['https://www.meitulu.com/t/miyu-suenaga/'], '末永みゆ')
+// index(['https://www.meitulu.com/t/miyu-suenaga/'], '末永みゆ')
+
+const urls = function(baseUrl, start=1, end) {
+  const list = []
+  for (let i = start; i <= end; i++) {
+    list.push(util.format(baseUrl, i))
+  }
+  return list
+}
+
+// todo 第一次直接访问带页码的url有爬虫限制
+// const u = urls('https://www.meitulu.com/t/1290/%s.html', 1, 3)
+const u = [
+  'https://www.meitulu.com/t/1290',
+  'https://www.meitulu.com/t/1290/2.html',
+  'https://www.meitulu.com/t/1290/3.html'
+]
+console.log(u);
+// index(u, '森萝财团')
+index(['https://www.meitulu.com/t/1374/'], '桜桃喵')
--- src/helper/getPicture.js
+++ src/helper/getPicture.js
@@ -5,9 +5,6 @@ const c = require('ansi-colors')
 
 const { saveImage, wait, mkdirSync, exists,writeFile } = require('./tools')
 
-
-const output = 'D:/Yui Aragaki/末永みゆ'
-
 // https://www.meitulu.com/t/miyu-suenaga/
 // e.g. https://mtl.gzhuibei.com/images/img/9910/1.jpg
 function makeList(start=1, end=30, format) {
@@ -35,8 +32,7 @@ function wrapList(list=[], dir='') {
   })
 }
 
-const { items } = require('./末永みゆ.json')
-function generatorList(list=[], name=Date.now()) {
+function generatorList(list=[], output, name=Date.now()) {
   const result = []
   for (const item of list) {
     let urls = makeList(1, item.p, item.url)
@@ -45,33 +41,55 @@ function generatorList(list=[], name=Date.now()) {
     result.push(...urls)
   }
 
-  writeFile({
-    fileName: name + '.json',
-    data: result,
-    output: __dirname
-  })
+  if (!exists(path.resolve(__dirname, name + '.json'))) {
+    writeFile({
+      fileName: name + '.json',
+      data: result,
+      output: __dirname
+    })
+  }
   return result
+}
+
+function filter(list=[]) {
+  return list.filter((item) => {
+    return !exists(path.resolve(item.dir, item.name))
+  })
 }
 
 /**
  * @description 输入图片url数组下载图片
  * @param {array} [list=[]]
- * @param {string} dir
+ * @param {number} delay
  */
-async function index(list=[], dir) {
+async function index(list=[], delay=100) {
+  list = filter(list)
   let len = list.length
-  let index = 1
+  let idx = 1
   for (const item of list) {
-    if (exists(path.resolve(item.dir, item.name))) {
-      console.log('exists', item.url)
-    } else {
-      await saveImage(item.url, item.dir, item.name.split('.')[0])
-      console.log(c.bgGreenBright(`${index++} / ${len}`))
-      await wait(1500)
-    }
+    await saveImage(item.url, item.dir, item.name.split('.')[0])
+    console.log(c.bgGreenBright(`${idx++} / ${len}`))
+    await wait(delay)
   }
 }
 
 // let list = wrapList(makeList(1, 30, '9910'))
-index(generatorList(items))
 
+function run(file, baseDir='D:/Yui Aragaki') {
+  const output = mkdirSync(path.resolve(baseDir, file)) // e.g. D:/Yui Aragaki/森萝财团
+  const { items } = require(`./${file}.json`) // e.g. require('./末永みゆ.json')
+  // http://xinsijitv78.top/tag/森萝财团.html
+
+  let genList = generatorList(items, output, `[${file}]pics`)
+
+  let n = items.reduce((acc, cur) => {
+    acc += cur.p
+    return acc
+  }, 0)
+  console.log(n)
+
+  // const list = require(`./[${file}]pics.json`)
+  index(genList, 100)
+}
+// run('森萝财团')
+run('桜桃喵')
