c3ac23cca2c855d563aa6c3d993dde2c524c7ff0
mrlhz <mr__lhz@163.com>
2019-12-19 23:56:50 +0800

    refactor: save city data database

--- src/CityParser/index.js
+++ src/CityParser/index.js
@@ -1,141 +1,149 @@
-const path = require('path')
-
-const puppeteer = require('puppeteer')
 const c = require('ansi-colors')
-require('module-alias/register')
 
-const { executablePath } = require('@config/index')
-const { proxyServer, auth } = require('@config/ip')
-const { sleep, writeFile } = require('@helper/tools')
+const { Browser } = require('../helper/browser')
+const { wait } = require('../helper/tools')
 const { getAreaHtml } = require('./getAreaHtml')
+const City = require('../models/city/city')
 
-let errs = [] // 存放出错的url
+const log = console.log
 
+function connect(dbName) {
+  const mongoose = require('mongoose')
+
+  const doubanDb = 'mongodb://localhost/' + dbName
+
+  mongoose.set('useFindAndModify', false) // https://mongoosejs.com/docs/deprecations.html
+
+  mongoose.connect(doubanDb, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }, () => log('mongodb connect success'))
+
+
+  // 让 mongoose 使用全局 Promise 库
+  mongoose.Promise = global.Promise
+  // 取得默认连接
+  const db = mongoose.connection
+
+  // 将连接与错误事件绑定（以获得连接错误的提示）
+  db.on('error', console.error.bind(console, 'MongoDB 连接错误：'))
+}
+
+async function insert(model, list) {
+  list = list.map((item) => setData(model, item))
+  async function setData(model, item) {
+    const m = await model.findOne({ id: item.id })
+    if (m) {
+      log(`${c.red('fail')}: ${item.name}(${item.id}) existed`)
+      return m
+    } else {
+      const res = await new model(item).save()
+      log(c.green('insert success:'), res.name, res.id)
+      return res
+    }
+  }
+
+  return Promise.all(list)
+}
+
+async function filter() {
+  // 值不为null且存在的记录
+  const list = await City.find({ todo: 1, url: { $ne:null, $exists: true } })
+  return list
+}
+
+/**
+ * @description 过滤一遍url，与数据库对比，已爬取的url不再发送请求
+ * @param {Array} todoList 
+ */
+async function check(todoList) {
+  const todoListUrls = todoList.map((item) => item.url)
+  const unfinished = await City.find({ todo: 1, url: { $in: todoListUrls }}) // todo 为0的urls
+  const unfinisheList = unfinished.map((item) => item.url)
+  const list = todoList.filter((item) => unfinisheList.includes(item.url))
+  return list
+}
+
+async function findPidList() {}
+
+async function todo(url) {
+  return await City.findOneAndUpdate({ url }, { $set: { todo: 0 } })
+}
+
+/**
+ * @description 
+ * @param {Array} list
+ * @returns
+ * @todo 
+ */
+async function getArea(list) {
+  const result = []
+  const browser = new Browser({})
+  let i = 1
+  const len = list.length
+  for (const item of list) {
+    const page = await browser.goto(item.url)
+    try {
+      let arr = await getAreaHtml(page) // should return array
+      if (Array.isArray(arr)) {
+        result.push(...arr)
+
+        await insert(City, arr)
+        await todo(item.url) // 成功访问的url todo值 => 0
+      } else if (arr.errMsg) {
+        log(c.bgRed(arr.errMsg))
+        continue
+      }
+
+      log(`${c.bgGreen('done')} ${i++} / ${len}`)
+    } catch (e) {
+      console.error(e)
+    }
+
+    await wait(3500)
+    await page.close()
+  }
+
+  await browser.close()
+
+  return result
+}
 
 /**
  * @description 获取 `2018年统计用区划代码和城乡划分代码`
  * @see http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2018/index.html
- * @param {puppeteer.browser} browser
- * @param {Function} fn
- * @param {Object} options
- * @returns
+ * @param {Array} list
  */
-async function getArea(browser, fn , options) {
-  let { urls, province } = options
-  if(!Array.isArray(urls)) {
-    console.log(urls, 'need Array')
-    return
-  }
-  let len = urls.length
-  let items = []
-  for (let i = 0; i < len; i++) {
-    let url = urls[i].url
-    if(url) {
-      const page = await browser.newPage()
-      // await page.authenticate(auth)
-      await page.setViewport({ width: 1920, height: 1200 })
-  
-      try {
-        console.log(`${c.green('fetch')} ${url}`)
-        await page.goto(url, {
-          timeout: 0,
-          waitUntil: 'networkidle2' // 当至少500毫秒的网络连接不超过2个时，考虑导航已经完成
-        })
-  
-        let item  = await fn(page)
-        try {
-          items.push(...item)
-          console.log(`${c.bgGreen('done')} ${(i + 1)}/${len}`)
-        } catch (e) {
-          console.log(`${c.bgRed('fail')} ${(i + 1)}/${len}`)
-          errs.push(urls[i])
-          console.log(e, i)
-        }
-        await page.close()
-      } catch (e) {
-        console.log(e)
-      }
-    }
-    await sleep(4000)
-  }
-  return items
-}
-
-
-async function index(urls, options) {
-  console.time('start')
-  let { name = Date.now(), province, output = __dirname } = options
-  let result = []
-  const browser = await puppeteer.launch({
-    headless: true,
-    executablePath,
-    ignoreHTTPSErrors: true,
-    // args: [proxyServer]
-  })
-
-  let list = urls
-  let names = ['city', 'county', 'town', 'village'].map((item) =>  province + '-' + item)
+async function index(list) {
+  const queue = []
+  queue.push(list)
+  let names = ['city', 'county', 'town', 'village']
   for (let i = 0; i < names.length; i++) {
-      list = await getArea(browser, getAreaHtml, {
-      urls: list,
-      province
-    })
-    await sleep(3000 + i * 600)
-    result.push(...list)
-    writeFile({
-      fileName: names[i] + '.json',
-      data: list,
-      output
-    })
+    let cur = queue.shift()
+    // 过滤
+    cur = await check(cur)
+    let item = await getArea(cur)
+
+    queue.push(item)
   }
 
-  writeFile({
-    fileName: province + '-errs.json',
-    data: errs,
-    output
-  })
-  await browser.close()
-  console.log(result.length)
-  writeFile({
-    fileName: province + '-result.json',
-    data: result,
-    output
-  })
-  console.timeEnd('start')
 }
-
 
 const pca = require('@area/province.min.json')
-// index(pca.slice(19,20), {
-//   name: 'gx',
-//   province: 'gx',
-//   output: path.resolve(__dirname, './data')
-// })
+let target = pca.filter((item) => item.name === '北京市')
 
-console.log(pca.length)
+async function run() {
+  // 连接数据库
+  await connect('city')
 
-// index(pca.slice(0,1), {
-//   name: 'beijing',
-//   province: 'beijing',
-//   output: path.resolve(__dirname, './data')
-// })
+  await index(target)
+  const list = await filter()
+  await getArea(list)
+  // await City.updateMany({ todo: 0 })
 
-// 北京 1+1+16+336 个页面，每个页面延迟5s，共耗时2291147.657ms
-// 天津 1+1+16+306 个页面，每个页面延迟5s，共耗时2523369.158ms
-// 广东 1+21+200+2597 个页面，每个页面延迟4s，共耗时
+  process.exit(0)
+}
 
-// index(pca.slice(1,2), {
-//   name: '天津市',
-//   province: '天津市',
-//   output: path.resolve(__dirname, './data')
-// })
-
-
-let target = pca.filter((item) => item.name === '广东省')
-
-console.log(target);
-// index(target, {
-//   name: '广东省',
-//   province: '广东省',
-//   output: path.resolve(__dirname, './data')
-// })
+const bj = require('./data/北京市.json')
+log(bj.length)
+// run()s
--- src/CityParser/readme.md
+++ src/CityParser/readme.md
@@ -15,3 +15,18 @@
 2. 获取某个省份 `index.js`
 
 ![2018年统计用区划代码和城乡划分代码(截止2018年10月31日)](../../images/getBJAreaData.png)
+
+```js
+
+// http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2018/11.html
+// http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2018/11/1101.html
+// http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2018/11/01/110101.html
+// http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2018/11/01/01/110101001.html
+```
+
+- 北京 1+1+16+336 个页面，每个页面延迟5s，共耗时2291147.657ms
+- 天津 1+1+16+306 个页面，每个页面延迟5s，共耗时2523369.158ms
+- 广东 1+21+200+2597 个页面，每个页面延迟4s，共耗时
+
+- 天津 5902
+- 北京 7507
