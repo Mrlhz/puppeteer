4df287ec6c1b8e9f1310d9fc274f5346de92b4da
mrlhz <mr__lhz@163.com>
2021-09-02 00:52:26 +0800

    optimize the code

--- tt/download.js
+++ tt/download.js
@@ -8,17 +8,20 @@ const { seriesSchema } = require('./models/series')
 const { movieSchema, starVideoSchema } = require('./models/javbus')
 const { wait, mkdirSync, exists  } = require('../src/helper/tools')
 const publicDirectory = 'D:/md/avmoo'
+const otherDirectory = '合集作品-JAVIdols' //  - 女優 - 影片
+const tuxiaoleDirectory = 'D:/md/avmoo/tuxiaole'
 const origin = 'https://www.busjav.blog'
 
 
-const db = connect('javbus')
 
 function formatUrl(url) {
   return url.includes('http') ? url : origin + url
 }
 
 async function init(params, options) {
-  const { series = '', type = 'starVideo' } = options
+  if (!options.series) { options.series = params.idols }
+  const db = connect('javbus')
+  const { series = '', type = 'starVideo', otherFolder = otherDirectory } = options
   const seriesOutput = path.resolve(publicDirectory, series)
   mkdirSync(seriesOutput) // 创建 演员 || 系列目录
   const data = await queryMovie(params, options)
@@ -35,12 +38,13 @@ async function init(params, options) {
   //   await downloadImage(JAVIdols, { ...options, seriesOutput: JAVIdolsOuput })
   // }
   if (JAVIdols.length) {
-    const JAVIdolsOuput = path.resolve(publicDirectory, `合集作品-JAVIdols`)
+    const JAVIdolsOuput = path.resolve(publicDirectory, otherFolder)
     mkdirSync(JAVIdolsOuput)
     await Promise.all([downloadImage(JAVIdol, { ...options, seriesOutput }), downloadImage(JAVIdols, { ...options, seriesOutput: JAVIdolsOuput })])
   } else {
     await downloadImage(JAVIdol, { ...options, seriesOutput })
   }
+  process.exit(0)
 }
 
 async function queryMovie(params, options) {
@@ -60,14 +64,14 @@ async function downloadImage(data = [], options = {}) {
   for (let index = 0; index < data.length; index++) {
     const { av, images } = data[index]
     console.log(av)
-    const fulterImages= images.filter(item => {
+    const filterImages= images.filter(item => {
       const imagePath = path.resolve(seriesOutput, av, item.name)
       return !exists(imagePath)
     })
     const output = path.resolve(seriesOutput, av)
     exists(output) ? '目录已存在' : mkdirSync(output) // 创建作品目录
 
-    const imagesPromise = fulterImages.map(item => {
+    const imagesPromise = filterImages.map(item => {
       return download(formatUrl(item.url), output, { filename: item.name })
     })
     if (all) {
@@ -83,21 +87,50 @@ async function downloadImage(data = [], options = {}) {
   console.log('total picture: ', allImages.length)
   const allSettledPromise = all && await Promise.allSettled(allImagesPromise)
   console.log(allSettledPromise.length, allImagesPromise.length, allImages.length)
+  console.log('end')
   return 0
 }
 
 function findStar(data = [], idols) {
-  const JAVIdols = [] // 多位演员
-  const JAVIdol = []  // 一位演员
+  const JAVIdols = [] // 合集作品，2个以上演员
+  const JAVIdol = []  // 一两个演员
   data.forEach(item => {
-    if (item.star.length === 1) {
+    if (item.star.length <= 2) {
       JAVIdol.push(item)
     } else {
       JAVIdols.push(item)
     }
   })
-  const list = data.filter(item => item.star.length === 1)
+  const list = data.filter(item => item.star.length <= 2)
   return { list, JAVIdol, JAVIdols }
+}
+
+// 标题
+function resolvePathName(name) {
+  return name.replace(/[\\\/\:\*\?\"\<\>\|]/g, ' ')
+}
+
+/**
+ * @description 单独下载图片
+ * @param {*} data
+ * @param {*} options
+ */
+async function downloadImages(data, options = {}) {
+  let { output = '', publicDir } = options
+  const imageOutput = path.resolve(publicDir || publicDirectory, resolvePathName(output))
+  mkdirSync(imageOutput) // 创建目录
+  const filterImages = data.filter(item => {
+    const imagePath = path.resolve(imageOutput, item.name)
+    console.log(item, imagePath)
+    return !exists(imagePath)
+  })
+  const imagesPromise = filterImages.map(item => {
+    const downloadOptions = item.name ? { filename: item.name } : {}
+    return download(item.url, imageOutput, downloadOptions)
+  })
+  console.log(imagesPromise.length)
+  await Promise.allSettled(imagesPromise)
+  console.log('end')
 }
 
 
@@ -113,4 +146,4 @@ function findStar(data = [], idols) {
 // init({ 'idols': 'さくらゆら' }, { series: 'さくらゆら', type: 'starVideo' })
 // init({ 'idols': '松本いちか' }, { series: '松本いちか', type: 'starVideo' }) // todo
 // init({ 'idols': '鈴村あいり' }, { series: '鈴村あいり', type: 'starVideo' })
-init({ 'idols': '明里つむぎ' }, { series: '明里つむぎ' })
+
