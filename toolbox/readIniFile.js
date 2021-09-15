dd72adfdad9d67869cadf2442afa2f6be49d8729
mrlhz <mr__lhz@163.com>
2021-09-16 00:21:21 +0800

    update read ini file

--- toolbox/readIniFile.js
+++ toolbox/readIniFile.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const http = require('http')
 
 const fs = require('fs-extra')
 const iconvLite = require('iconv-lite')
@@ -9,37 +8,52 @@ async function getIniFile(dir) {
   return files.map(item => path.resolve(dir, item, 'desktop.ini')).filter(item => fs.pathExistsSync(item))
 }
 
+/**
+ * @see https://github.com/ashtuchkin/iconv-lite
+ * @param {String} file
+ * @returns
+ */
 function read(file) {
   const text = fs.readFileSync(file)
   return iconvLite.decode(text, 'gbk')
 }
 
-// async function init() {
-//   const list = await getIniFile('E:\\bilibili\\鬼畜')
-
-//   const server = http.createServer((err, res) => {
-//     const text = read(list[0])
-//     console.log(text.toString('utf8'))
-//     // res.writeHead(200, {'Content-Type': 'text/html;charset=UTF8'})
-//     res.end(text)
-//   })
-
-//   server.listen(3000, '127.0.0.1')
-// }
-
-async function init() {
-  const list = await getIniFile('E:\\bilibili\\鬼畜')
+async function compilerIniText(src) {
+  const list = await getIniFile(src)
   const data = list.map(item => {
     const text = read(item)
-    // console.log(text.split('\n')[1].split('=')[1])
-    const title = text.match(/InfoTip=(.*?)\n/)
+    const title = text.match(/InfoTip=(.*?)\n/) // console.log(text.split('\n')[1].split('=')[1])
     return {
       title: title ? title[1] : '',
       path: item,
       dir: path.dirname(item)
     }
   })
-  console.log(data)
+  return data
 }
 
-init()
+function generateJSON(filePath, data) {
+  if (!fs.pathExistsSync(filePath)) return
+
+  const file = path.resolve(filePath, `list.json`)
+  fs.outputJSONSync(file, data)
+}
+
+function generateMarkdown(filePath, data) {
+  if (!fs.pathExistsSync(filePath)) return
+
+  const file = path.resolve(filePath, `list.md`)
+  const text = data.map(item => `- ${item.title}`).join('\n')
+  fs.outputFileSync(file, text)
+}
+
+async function init(src) {
+  const data = await compilerIniText(src)
+  generateJSON(src, data)
+  generateMarkdown(src, data)
+  console.log(data[0])
+}
+
+
+
+init('E:\\bilibili\\鬼畜')
