e91fafa187938d05ae0a2bd406bf29428a1ec7d9
mrlhz <mr__lhz@163.com>
2021-09-19 15:25:55 +0800

    update read ini file

--- toolbox/readIniFile.js
+++ toolbox/readIniFile.js
@@ -1,9 +1,9 @@
 const path = require('path')
 
 const fs = require('fs-extra')
-const iconvLite = require('iconv-lite')
+const iconv = require('iconv-lite')
 
-async function getIniFile(dir) {
+async function getIniFilePath(dir) {
   let files = await fs.readdir(dir)
   return files.map(item => path.resolve(dir, item, 'desktop.ini')).filter(item => fs.pathExistsSync(item))
 }
@@ -13,23 +13,35 @@ async function getIniFile(dir) {
  * @param {String} file
  * @returns
  */
-function read(file) {
-  const text = fs.readFileSync(file)
-  return iconvLite.decode(text, 'gbk')
+ function readIniFile(file) {
+  const bufferText = fs.readFileSync(file)
+  const result = iconv.decode(Buffer.from(bufferText), 'gbk')
+  return result
+}
+
+async function getIniFileText(src) {
+  const list = await getIniFilePath(src)
+  return list.map(item => {
+    return {
+      file: item,
+      text: readIniFile(item)
+    }
+  })
 }
 
 async function compilerIniText(src) {
-  const list = await getIniFile(src)
-  const data = list.map(item => {
-    const text = read(item)
+  const textList = await getIniFileText(src)
+  const result = textList.map(item => {
+    const text = item.text
     const title = text.match(/InfoTip=(.*?)\n/) // console.log(text.split('\n')[1].split('=')[1])
     return {
       title: title ? title[1] : '',
-      path: item,
-      dir: path.dirname(item)
+      path: item.file,
+      dir: path.dirname(item.file),
+      text
     }
   })
-  return data
+  return result
 }
 
 function generateJSON(filePath, data) {
@@ -47,13 +59,27 @@ function generateMarkdown(filePath, data) {
   fs.outputFileSync(file, text)
 }
 
+async function writeTxtFile(src) {
+  const list = await getIniFileText(src)
+  const taskList = list.map(item => {
+    const { dir, name } = path.parse(item.file)
+    const outputFilePath = path.resolve(dir, `${name}.txt`)
+    return fs.outputFile(outputFilePath, item.text, { encoding: 'utf8' })
+  })
+  await Promise.allSettled(taskList)
+}
+
 async function init(src) {
   const data = await compilerIniText(src)
   generateJSON(src, data)
   generateMarkdown(src, data)
+  await writeTxtFile(src)
   console.log(data[0])
 }
 
 
 
-init('E:\\bilibili\\鬼畜')
+// init('E:\\bilibili\\鬼畜')
+// init('E:\\bilibili\\电视剧')
+init('E:\\bilibili\\电影')
+// readIniFile('E:\\bilibili\\电影\\84264321\\desktop.ini')
